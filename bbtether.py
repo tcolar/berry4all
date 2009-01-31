6ef43aff468d3827fb6c72995b0e55ce678f7161
tcolar <devnull@localhost>
2009-01-31 19:55:34 +0000

    Some cleanup

--- bbtether.py
+++ bbtether.py
@@ -26,6 +26,7 @@ import threading
 import array
 
 VERSION="0.1"
+DEBUG=True
 
 VENDOR_RIM=0x0fca
 PRODUCT_DATA=0x0001   #(older bberry)
@@ -42,14 +43,16 @@ COMMAND_PIN = [0x00,0x00,0x0c,0x00,0x05,0xff,0x00,0x00,0x00,0x00,0x04,0x00]
 COMMAND_DESC= [0x00,0x00,0x0c,0x00,0x05,0xff,0x00,0x00,0x00,0x00,0x02,0x00]
 COMMAND_HELLO=[0x00, 0x00, 0x10, 0x00, 0x01, 0xff, 0x00, 0x00,0xa8, 0x18, 0xda, 0x8d, 0x6c, 0x02, 0x00, 0x00]
 #MODE_DESKTOP_PACKET=[0x00, 0x00, 0x18, 0x00, 0x52, 0x49, 0x4d, 0x20, 0x44, 0x65, 0x73, 0x6b, 0x74, 0x6f, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00]  # "RIM Desktop"
-#OPEN_SOCKET = [0x00, 0x00, 0x07, 0x00, 0x0a, 0x08, 0x00]
-MODEM_ACK2 = [0x00, 0x00, 0x00, 0x00, 0xb0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc2, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12]
-MODEM_ACK3 = [0x4, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x1c, 0x0, 0x0, 0x0, 0x78, 0x56, 0x34, 0x12 ]
-MODEM_ACK4 = [0x0, 0x0, 0x0, 0x0, 0x20, 0x0, 0x0, 0x0, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  0x18, 0x0, 0x0, 0x0, 0x78, 0x56, 0x34, 0x12 ]
-MODEM_ACK = [0x01, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12]
+MODEM_START = [0x01, 0x00, 0x00, 0x00, 0x78, 0x56, 0x34, 0x12]
+MODEM_ACK = [0x4, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, 0x0, 0x0, 0x0, 0x1c, 0x0, 0x0, 0x0, 0x78, 0x56, 0x34, 0x12 ]
+MODEM_ACK2 = [0x0, 0x0, 0x0, 0x0, 0x20, 0x0, 0x0, 0x0, 0x20, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  0x18, 0x0, 0x0, 0x0, 0x78, 0x56, 0x34, 0x12 ]
 
 
 ''' 'Static' Methods '''
+def debug(msg):
+	if(DEBUG):
+		print msg
+	
 def tuple_to_hex(tuple, msg):
 	text=""
 	hexa=""
@@ -63,7 +66,7 @@ def tuple_to_hex(tuple, msg):
 			text+="."
 		cpt+=1
 		if(cpt%16==0 or cpt==len(tuple)):
-			print msg,"[",hexa,"] [",text,"]"
+			debug(msg+"["+hexa+"] ["+text+"]")
 			text=""
 			hexa=""
 
@@ -257,9 +260,6 @@ class BBTether:
 				# Modem use does not require to be in desktop mode, so don't do it.
 				#self.enable_desktop_mode(handle,writept,readpt)
 
-				#handle.clearHalt(readpt)
-				#handle.clearHalt(writept)
-
 				modem=BBModem(handle, readpt, writept)
 				modem.start()
 			
@@ -281,33 +281,21 @@ class BBModem:
 	def start(self):
 		#open modem PTY
 		(master,slave)=pty.openpty()
-		print "\nModem pty: ",os.ttyname(master)," - ",os.ttyname(slave)
-		
-		#self.handle.bulkWrite(self.writept, MODEM_ACK, TIMEOUT)
-		
-		#os._exit(0)
-		
-		#readThread=BBModemReadThread(self.handle,self.writept,master)
-		#readThread.start()	
-		
-		#writeThread=BBModemWriteThread(self.handle,self.readpt,slave)
-		#writeThread.start()	
-		
-		bbThread=BBModemThread(self.handle,self.readpt,self.writept,master,slave)
+		print "\nModem pty: ",os.ttyname(slave)
+				
+		bbThread=BBModemThread(self.handle,self.readpt,self.writept,master)
 		bbThread.start()
 		
 		print "Initializing Modem"
-		modem_write(self.handle,self.writept, MODEM_ACK)
+		modem_write(self.handle,self.writept, MODEM_START)
+		print "Modem Ready at ",os.ttyname(slave)
 		
 		try:
-#			time.sleep(3600*24)#24 hours
+			# Read from PTY and write to USB modem
 			while(True):
-				#print "."
 				bytes=os.read(master, 2000)
 				if(len(bytes)>0):
-					print "read ",len(bytes)," bytes ",bytes
 					modem_write(self.handle,self.writept, bytes)
-				#time.sleep(0.1)
 				
 		except KeyboardInterrupt:
 			print "\nShutting down on ^C"
@@ -320,9 +308,8 @@ class BBModem:
 class BBModemThread( threading.Thread ):
 	
 	
-	def __init__(self, hnd, read, write, master, slave):
+	def __init__(self, hnd, read, write, master):
 		threading.Thread.__init__(self)
-		self.input=master
 		self.output=master
 		self.handle=hnd
 		self.writept=write
@@ -336,14 +323,14 @@ class BBModemThread( threading.Thread ):
 		print "Starting Modem thread"
 		while( not self.done):
 			try:
-				#print "*"
 				try:
+					# Reda from USB modem and write to PTY
 					bytes=modem_read(self.handle, self.readpt)
 					if(len(bytes)>0):
-						if(same_tuple(bytes,MODEM_ACK3) or same_tuple(bytes,MODEM_ACK4)):
-							print "Received ACK"
+						if(same_tuple(bytes,MODEM_ACK) or same_tuple(bytes,MODEM_ACK2)):
+							# We do not write "ACK" messages from bberry (useless)
+							pass
 						else:
-							print "write ",len(bytes)," bytes ",bytes
 							data=array.array("B",bytes)
 							os.write(self.output,data.tostring())
 				except usb.USBError, error:
@@ -356,55 +343,6 @@ class BBModemThread( threading.Thread ):
 				else:
 					raise
 	
-	
-'''Read from PTY and write to USB (to modem)'''		
-class BBModemReadThread( threading.Thread ):
-	input=None
-	handle=None
-	writept=None
-	
-	def __init__(self, hnd, write, inp):
-		threading.Thread.__init__(self)
-		self.input=inp
-		self.handle=hnd
-		self.writept=write
-
-	def run (self):
-		print "Starting Modem read thread"
-		while(True):
-			bytes=os.read(self.input, 2000)
-			if(len(bytes)>0):
-				print "read ",len(bytes)," bytes ",bytes
-				modem_write(self.handle,self.writept, bytes)
-				time.sleep(1)
-
-'''read from USB and write to PTY (from modem)'''		
-class BBModemWriteThread( threading.Thread ):
-	output=None
-	handle=None
-	readpt=None
-	
-	def __init__(self, hnd, read, out):
-		threading.Thread.__init__(self)
-		self.output=out
-		self.handle=hnd
-		self.readpt=read
-				
-	def run (self):
-		print "Starting Modem write thread"
-		while(True):
-			try:
-				bytes=modem_read(self.handle, self.readpt)
-				if(len(bytes)>0):
-					print "write ",len(bytes)," bytes ",bytes
-					#str=''.join(bytes)
-					#os.write(self.output,str)
-					data=array.array("B",bytes)
-					os.write(self.output,data.tostring())
-			except usb.USBError, error:
-				#print "usb error: ",error
-				toto=2
-
 
 # MAIN
 BBTether()
