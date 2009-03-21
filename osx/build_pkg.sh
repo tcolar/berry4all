b5948b8cfcc587c8803fa7f859a8fe3a61be3307
tcolar <devnull@localhost>
2009-03-21 01:12:39 +0000

    osx

--- osx/build_pkg.sh
+++ osx/build_pkg.sh
@@ -8,16 +8,21 @@ ROOT=PACK_ROOT
 #copy precompiled pyusb in resources folder
 #sudo cp -rf pyusb-0.4.1 Resources
 
-rm -rf ${PACKAGE}.pkg
-rm ${PACKAGE}.pkg.*
+rm -rf ${PACKAGE}-*.pkg
+rm ${PACKAGE}-*.pkg.*
 find ${ROOT} -name .DS_Store -delete
-find Resources -name .DS_Store -delete
-/Developer/Tools/packagemaker -build -f ${ROOT} -p ${PACKAGE}.pkg -i Info.plist \
-	-d Description.plist -r Resources -ds -v
+find Resources-tiger -name .DS_Store -delete
+find Resources-leopard -name .DS_Store -delete
+/Developer/Tools/packagemaker -build -f ${ROOT} -p ${PACKAGE}-tiger.pkg -i Info.plist \
+	-d Description.plist -r Resources-tiger -ds -v
+/Developer/Tools/packagemaker -build -f ${ROOT} -p ${PACKAGE}-leopard.pkg -i Info.plist \
+	-d Description.plist -r Resources-leopard -ds -v
 
 #chown -R root:wheel ${PACKAGE}.pkg
-tar -h ${PACKAGE}.pkg -cf ${PACKAGE}.pkg.tar
-gzip -9 ${PACKAGE}.pkg.tar
+tar -h ${PACKAGE}-tiger.pkg -cf ${PACKAGE}-tiger.pkg.tar
+gzip -9 ${PACKAGE}-tiger.pkg.tar
+tar -h ${PACKAGE}-leopard.pkg -cf ${PACKAGE}-leopard.pkg.tar
+gzip -9 ${PACKAGE}-leopard.pkg.tar
 
 #test: sudo /usr/sbin/installer -verbose -pkg pyusb-0.41.pkg -target /
 
