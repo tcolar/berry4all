c16557902e077459476fa6ed4b4567cdcef4a94d
tcolar <devnull@localhost>
2009-10-11 19:34:37 +0000

    snow-leopard

--- osx/build_pkg.sh
+++ osx/build_pkg.sh
@@ -4,6 +4,7 @@
 
 PACKAGE=pyusb-0.41
 ROOT=PACK_ROOT
+PMAKER=/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker
 
 #copy precompiled pyusb in resources folder
 #sudo cp -rf pyusb-0.4.1 Resources
@@ -13,16 +14,22 @@ rm ${PACKAGE}-*.pkg.*
 find ${ROOT} -name .DS_Store -delete
 find Resources-tiger -name .DS_Store -delete
 find Resources-leopard -name .DS_Store -delete
-/Developer/Tools/packagemaker -build -f ${ROOT} -p ${PACKAGE}-tiger.pkg -i Info.plist \
+find Resources-snow-leopard -name .DS_Store -delete
+${PMAKER} -build -f ${ROOT} -p ${PACKAGE}-tiger.pkg -i Info.plist \
 	-d Description.plist -r Resources-tiger -ds -v
-/Developer/Tools/packagemaker -build -f ${ROOT} -p ${PACKAGE}-leopard.pkg -i Info.plist \
+${PMAKER} -build -f ${ROOT} -p ${PACKAGE}-leopard.pkg -i Info.plist \
 	-d Description.plist -r Resources-leopard -ds -v
 
+${PMAKER} -build -f ${ROOT} -p ${PACKAGE}-snow-leopard.pkg -i Info.plist \
+	-d Description.plist -r Resources-snow-leopard -ds -v
+
 #chown -R root:wheel ${PACKAGE}.pkg
-tar -h ${PACKAGE}-tiger.pkg -cf ${PACKAGE}-tiger.pkg.tar
+tar -cf ${PACKAGE}-tiger.pkg.tar ${PACKAGE}-tiger.pkg 
 gzip -9 ${PACKAGE}-tiger.pkg.tar
-tar -h ${PACKAGE}-leopard.pkg -cf ${PACKAGE}-leopard.pkg.tar
+tar -cf ${PACKAGE}-leopard.pkg.tar ${PACKAGE}-leopard.pkg
 gzip -9 ${PACKAGE}-leopard.pkg.tar
+tar  -cf ${PACKAGE}-snow-leopard.pkg.tar ${PACKAGE}-snow-leopard.pkg
+gzip -9 ${PACKAGE}-snow-leopard.pkg.tar
 
 #test: sudo /usr/sbin/installer -verbose -pkg pyusb-0.41.pkg -target /
 
