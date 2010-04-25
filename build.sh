120c9fa3aef978cf1beacdfe341b56a277a7ca12
tcolar <tcolar@tcolar-laptop>
2010-04-25 00:30:08 -0700

    0.3l

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+mkdir dist
 rm -f src/bbtether/*.pyc
 cd src
-tar czvf ../bbtether.tgz bbtether
+tar czvf ../dist/bbtether.tgz bbtether
 cd ..
 pylint -e src/bbtether/*py
 pychecker src/bbtether/*py
