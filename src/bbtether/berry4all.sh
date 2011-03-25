34660fba3c6b8033dfae7f27a1e92541dfa08cf8
tcolar <devnull@localhost>
2011-03-25 09:43:32 -0700

    Repackaged OSX libraries correctly (zip) Fixed berry4all script t use 32 bits python n SX to make wxpython happy

--- src/bbtether/berry4all.sh
+++ src/bbtether/berry4all.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+os=`uname`
+if [[ "$os" == 'Darwin' ]]
+then
+	echo "OSX: Will force use of 32 bits python as it's needed for 32 bits wxpython to work."
+	export VERSIONER_PYTHON_PREFER_32_BIT=yes
+fi
+
 python bbgui.py
