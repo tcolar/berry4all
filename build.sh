106d05d0ee8b54512577c05bd2e9b865f641dbf1
tcolar <devnull@localhost>
2011-03-25 09:55:14 -0700

    build script generates zip archive too (osx likes that better)

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ mkdir dist
 rm -f src/bbtether/*.pyc
 cd src
 tar czvf ../dist/bbtether.tgz bbtether
+zip -r ../dist/bbtether.zip bbtether
 cd ..
 #pylint -e src/bbtether/*py
 #pychecker src/bbtether/*py
