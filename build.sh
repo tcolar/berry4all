48e35d02294690e7e618e6da616509f3c8007ffa
tcolar <devnull@localhost>
2011-03-25 09:47:59 -0700

    chglog

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ rm -f src/bbtether/*.pyc
 cd src
 tar czvf ../dist/bbtether.tgz bbtether
 cd ..
-pylint -e src/bbtether/*py
-pychecker src/bbtether/*py
+#pylint -e src/bbtether/*py
+#pychecker src/bbtether/*py
